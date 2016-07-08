741875564cf3f9c7128172cbe608bdf37b0a0f9c
Daniel J Janiak <danieljjaniak@Daniels-Air.attlocal.net>
2016-07-07 21:52:45 -0700

    Create UI for homescreen and detail view, and segue

---
+++ favorite-place/DetailVC.swift
@@ -0,0 +1,39 @@
+//
+//  DetailVC.swift
+//  favorite-place
+//
+//  Created by Daniel J Janiak on 3/17/16.
+//  Copyright © 2016 Daniel J Janiak. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backPressed(sender: AnyObject) {
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
